d8a8741c824454255ab6ac11b88ce5f9bc2105e3
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2023-11-08 13:05:22 +0400

    Adds wrapper for project sections, semantic tokens

--- .eleventy.shortcodes.js
+++ .eleventy.shortcodes.js
@@ -1,37 +1,43 @@
 const markdownIt = require("markdown-it");
 
 module.exports = config => {
-	config.setLibrary("md", markdownIt({
+	const md = markdownIt({
 		html: true,
 		breaks: true,
 		linkify: true
-	  }));
+	});
+	config.setLibrary("md", md);
 
 
 	// MD in NJK
 	config.addPairedShortcode("md", (content) => {
-		const md = new markdownIt();
-  		return md.render(content.replace(/^\s+/gm, ''));
+		return md.render(content.replace(/^\s+/gm, ''));
 	});
 
 
+	// 
+	config.addPairedShortcode("projectSection", function(content, title) {
+	const renderedContent = md.render(content);
+
+
+	return `<div class="project-section">
+		<h2 class="project-section__heading">${title}</h2>
+		${renderedContent}
+		</div>`;
+	});
+
+
+	// currentYear
 	config.addShortcode("currentYear", () => `${new Date().getFullYear()}`);
 
 	// Projects
 	config.addPairedShortcode("projectColumn", function(content) {
 		return `
-			<div class="project-desc__column">
+			<div class="project-body__column">
 				${content}
 			</div>
 			`;
 	});
 
-	config.addPairedShortcode("projectDesc", function(content, title) {
-		return `
-			<div>
-				<h2 class="project-desc__heading">${title}</h2>
-				${content}
-			</div>
-			`;
-	});	
+	  
 }
