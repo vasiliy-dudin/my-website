4d922dc08973ae5801cbdf166da880fac36b496e
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2026-06-27 16:00:43 +0100

    fix: Resolve watch task error

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,4 @@
-import yaml from "js-yaml";
+import * as yaml from "js-yaml";
 import pluginImages from "./eleventy.images.js";
 import pluginShortcodes from "./eleventy.shortcodes.js";
 import pluginFilters from "./eleventy.filters.js";
